68e9002e83022b555748c106e88b0ddcc7cd6ef0
Justin <just.coder@outlook.com>
2023-09-25 19:31:26 +0100

    updated heading and colour

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 updateBG = () => {
   // console.log(document.querySelector("body").style.backgroundColor);
   const x = document.querySelector("body").style.backgroundColor;
-  document.querySelector("body").style.backgroundColor = (x === "tomato") ? "azure" : "tomato";
+  document.querySelector("body").style.backgroundColor = (x === "lightgoldenrodyellow") ? "azure" : "lightgoldenrodyellow";
 }
 
 document.querySelector("#myBtn").addEventListener("click", updateBG);
